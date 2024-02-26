5b8124481f2122cf0996c431b2745afac395326e
Karl <smitzkar@gmail.com>
2024-02-26 13:37:22 +0100

    Adjust boot.py, not sure if better

--- python/boot.py
+++ python/boot.py
@@ -14,16 +14,23 @@ wifi_password = 'Rlb_Karl<3'
 sta_if = network.WLAN(network.STA_IF)
 sta_if.active(True)
 
-for i in range(5):
-    if not sta_if.isconnected():
-        print('connecting to network...')
+led = Pin(13, Pin.OUT)
+
+# Try to connect to Wi-Fi (and start webrepl) 5 times
+for i in range (5):
+    print('connecting to network...')
+    try:
         sta_if.connect(wifi_ssid, wifi_password)
-        time.sleep(5)
-    else:
-        try:
-            pin = Pin(13, Pin.OUT)
-            pin.on()
-            webrepl.start() # Seems to start on 'ip_address:8266'
-        except Exception as e:
-            print('An error occurred:', e)
+        webrepl.start() # tried to pull it out of here, but it didn't work, so this'll have to do
+        led.on()
         break
+    # If it fails for some reason, blink a bit and try again
+    except Exception as e:
+        print('An error occurred:', e)
+        for j in range(3):
+            led.on()
+            time.sleep(1)
+            led.off()
+            time.sleep(1)
+        continue
+
