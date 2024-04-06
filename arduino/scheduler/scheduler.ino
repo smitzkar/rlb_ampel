b76153c047bfcc3b23cb14290a6340ceefbf0854
Karl <smitzkar@gmail.com>
2024-04-06 02:27:37 +0200

    Finally fix the delayUntil function

--- arduino/scheduler/scheduler.ino
+++ arduino/scheduler/scheduler.ino
@@ -6,8 +6,8 @@
 
 #define LED_PIN 13 // for the onboard LED
 
-const char* ssid = "Karl";
-const char* password = "Rlb_KsESP";
+const char* ssid = "Rlb_Ampel";
+const char* password = "Rlb_Ampel<3";
 const char* ntpServer = "0.pool.ntp.org";
 const long  gmtOffset_sec = 3600;       // Offset for your timezone in seconds
 const int   daylightOffset_sec = 3600;  // Offset for daylight saving time in seconds
@@ -34,85 +34,48 @@ int totalBlinks = 0; // Keep track of the total number of blinks
 int tolerance = 3; // Tolerance for syncing to the start of the minute in seconds
 
 // For trial to start at specific time 
-bool startAtSpecificTime = false;
-int startHour = 0;
-int startMinute = 0;
+bool startAtSpecificTime = true;
+int startHour = 2;
+int startMinute = 25;
 
 
-void setup()
-{
-
-  // WARNING: do NOT use this on the inbuilt ESP
-  pinMode(LED_PIN, OUTPUT); // Set the LED pin as output
 
 
-  // Connect to WiFi
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-  }
-
-  Serial.begin(115200);   // for output to serial monitor
-
-  // Configure NTP and sends first request for syncing the time (actually runs asynchronously in the background, handled by FreeRTOS?)
-  // will be 01.01.1970 01:00:00 on first Serial.println(ctime(&now)); (not worth the effort to fix this, as it's just a minor inconvenience. Fixes itself on first update.)
-  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
-  // Wait until time is updated (not the epoch time)
-  while (time(nullptr) <= 60 * 60 * 24) { // 60*60*24 is the number of seconds in a day
-    delay(1000); // wait for 1 second before trying again
-  }
-}
-
-void loop()
-{
+// Checks current offset from the start of the minute. If within tolerance, returns the offset. Otherwise, delays until the start of the next minute. 
+int syncToMinute(int tolerance) {
   time_t now;
-  time(&now); // Call the time function, which checks the current time and stores it in the variable now (we pass the address to the function, so it can both access and modify the variable). Is C stuff, just go with it for now :)
+  time(&now);
+  struct tm* currentTime = localtime(&now);
+  int seconds = currentTime->tm_sec;
 
-  // Adds a 30s buffer to the start time, so that the first cycle starts at the specified time. 
-  // (as long as the sync to full minute function is also used!)
-  if (startAtSpecificTime) {
-    if (startMinute == 0) {
-      startMinute = 59;
-      // still getting used to ternary operator. 
-      // startHour is set to:(if startHour is 0, set it to 23, otherwise set it to startHour - 1)
-      startHour = startHour == 0 ? 23 : startHour - 1; 
-      delayUntil(startHour, startMinute, 30); 
-    } else {
-      startMinute -= 1;
-      delayUntil(startHour, startMinute, 30); 
+  if (tolerance < seconds && seconds < (60 - tolerance)) {
+    // Calculate the remaining seconds and milliseconds in the current minute
+    int remainingSeconds = 59 - seconds;
+    long remainingMillis = 1000 - (millis() % 1000);
+    Serial.print("Syncing to minute. Starting in:");
+    Serial.println(remainingSeconds);
+
+    // Delay for the remaining time
+    delay(remainingSeconds * 1000 + remainingMillis);
+    Serial.println("Synced to minute");
+    return 0;
+  }
+  // Could do this with ternary, but it's 2:55 am
+  else {
+    if (seconds <= 30){
+      return seconds;
+    }
+    else {
+      return 60 - seconds;
     }
   }
-
-  // Update the time from the NTP server every ntpUpdateInterval minutes
-  if (localtime(&now)->tm_min % ntpUpdateInterval == 0 && lastNtpUpdate != localtime(&now)->tm_min) {
-    configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
-    lastNtpUpdate = localtime(&now)->tm_min;
-    Serial.println(ctime(&now)); // Print the current time to the serial monitor
-    Serial.println("Blinks during last 5min: ");
-    Serial.println(blinks); // Print the number of blinks during the last 5 minutes
-    totalBlinks += blinks; // Add the number of blinks to the total
-    blinks = 0; // Reset the number of blinks
-    Serial.println("Total blinks: ");
-    Serial.println(totalBlinks); // Print the total number of blinks
-
-    syncToMinute(tolerance); // Delay until just before the start of the next full minute
-  }
-
-  Serial.println("Starting new cycle of blinking at ");
-  Serial.println(ctime(&now));
-  
-
-  blinkCycle(fastBlinkCount, slowBlinkCount, fastBlinkDuration, slowBlinkDuration);
-  
 }
 
 
 void blinkCycle(int fastBlinkCount, int slowBlinkCount, int fastBlinkDuration, int slowBlinkDuration) {
   // Blink the LED fast for fastBlinkDuration seconds
-  Serial.println("Fast blink actual runtime:");
   Serial.println(blinkControl(fastBlinkCount, fastBlinkDuration));
   // Blink the LED slow for slowBlinkDuration seconds
-  Serial.println("Slow blink actual runtime:");
   Serial.println(blinkControl(slowBlinkCount, slowBlinkDuration));
 }
 
@@ -181,48 +144,103 @@ unsigned long blinkControl(int numberOfBlinks, unsigned long duration) {
     delay(blinkInterval / 2); 
     blinks += 1;
   }
+  Serial.println("Blink cycle runtime:");
   return millis() - startTime; // return the total runtime of the function
 }
 
-// Checks current offset from the start of the minute. If within tolerance, returns the offset. Otherwise, delays until the start of the next minute. 
-int syncToMinute(int tolerance) {
-  time_t now;
-  time(&now);
-  struct tm* currentTime = localtime(&now);
-  int seconds = currentTime->tm_sec;
-
-  if (tolerance < seconds && seconds < (60 - tolerance)) {
-    // Calculate the remaining seconds and milliseconds in the current minute
-    int remainingSeconds = 59 - seconds;
-    long remainingMillis = 1000 - (millis() % 1000);
-    Serial.println("Syncing to minute");
-    Serial.println(remainingSeconds);
-
-    // Delay for the remaining time
-    delay(remainingSeconds * 1000 + remainingMillis);
-    Serial.println("Synced to minute");
-    return 0;
-  }
-  // Could do this with ternary, but it's 2:55 am
-  else {
-    if (seconds <= 30){
-      return seconds;
-    }
-    else {
-      return 60 - seconds;
-    }
-  }
-}
-
-// Delays until specified time
-void delayUntil(int targetHour, int targetMinute) {
+// Delays until specified time, with optional buffer in seconds (don't use a buffer larger than 59 seconds. I could write some code handling this case, but this buffer is hardcoded and not accessible to the user, so dear reader... if you want a larger buffer, implement it yourself!).
+void delayUntil(int targetHour, int targetMinute, int secondsBuffer = 0) {
   time_t now;
   struct tm* currentTime;
+
+  int targetSecond = 0; 
+
+  // Adjusts the target time if a buffer is specified
+  if (secondsBuffer > 0) {
+    targetSecond = 60 - secondsBuffer - 1; // not sure why, but it kept being off by one second, so I'm compensating for that here
+    if (targetMinute == 0) {
+      targetMinute = 59;
+      // still getting used to ternary operator. 
+      // startHour is set to:(if startHour is 0, set it to 23, otherwise set it to startHour - 1)
+      targetHour = targetHour == 0 ? 23 : targetHour - 1; 
+    } else {
+      targetMinute -= 1;
+    }
+  }
 
   do {
     time(&now);
     currentTime = localtime(&now);
-  } while(currentTime->tm_hour != targetHour || currentTime->tm_min != targetMinute);
+    Serial.println("Waiting for target time...");
+    delay(1000);
+  } while(!(currentTime->tm_hour == targetHour && currentTime->tm_min == targetMinute && currentTime->tm_sec == targetSecond));
 
-  // Execution will continue here at the target time (ur just use it to delay the loop)
+  startAtSpecificTime = false; // Reset the flag, so that the loop doesn't get stuck in this function
+
+  // Execution will continue here at the target time (or just use it to delay the loop)
+}
+
+
+void setup()
+{
+
+  // WARNING: do NOT use this on the inbuilt ESP
+  pinMode(LED_PIN, OUTPUT); // Set the LED pin as output
+
+
+  // Connect to WiFi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+  }
+
+  Serial.begin(115200);   // for output to serial monitor
+
+  // Configure NTP and sends first request for syncing the time (actually runs asynchronously in the background, handled by FreeRTOS?)
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+  // Wait until time is actually updated
+  time_t now;
+  time(&now);
+  while (localtime(&now)->tm_year == 70 ) {
+    time(&now);
+    delay(1000); 
+  }
+
+  time(&now); 
+  Serial.println(ctime(&now)); // Print the current time to the serial monitor
+}
+
+void loop()
+{
+  time_t now;
+  time(&now); // Call the time function, which checks the current time and stores it in the variable now (we pass the address to the function, so it can both access and modify the variable). Is C stuff, just go with it for now :)
+
+  // Update the time from the NTP server every ntpUpdateInterval minutes
+  if (localtime(&now)->tm_min % ntpUpdateInterval == 0 && lastNtpUpdate != localtime(&now)->tm_min) {
+    configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+    lastNtpUpdate = localtime(&now)->tm_min;
+    Serial.println(ctime(&now)); // Print the current time to the serial monitor
+    Serial.println("Blinks during last 5min: ");
+    Serial.println(blinks); // Print the number of blinks during the last 5 minutes
+    totalBlinks += blinks; // Add the number of blinks to the total
+    blinks = 0; // Reset the number of blinks
+    Serial.println("Total blinks: ");
+    Serial.println(totalBlinks); // Print the total number of blinks
+
+    syncToMinute(tolerance); // Delay until just before the start of the next full minute
+  }
+
+  // Adds a 30s buffer to the start time, so that the first cycle starts at the specified time. 
+  // (as long as the sync to full minute function syncToMinute is also used! currently called by blinkControl)
+  if (startAtSpecificTime) {
+    delayUntil(startHour, startMinute, 20); 
+  }
+
+  Serial.println("Starting new cycle of blinking at ");
+  time(&now);
+  Serial.println(ctime(&now));
+  
+
+  blinkCycle(fastBlinkCount, slowBlinkCount, fastBlinkDuration, slowBlinkDuration);
+  
 }
